7afc5a9d094db53465467a8b7afcb311eaf02611
JeffButler <butler.jeff1@gmail.com>
2017-07-10 19:47:15 -0700

    Added Sever Deletion capability

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,13 @@ class Car < ApplicationRecord
   validates_presence_of :model, :car_type, :color, :photo_id, :year
   has_many :accidents
   has_one :drivers
+
+  before_destroy :destroy_drivers_and_accidents
+
+   private
+
+   def destroy_drivers_and_accidents()
+     Accident.where(car_id: self.id).destroy_all
+     Driver.where(car_id: self.id).destroy_all
+   end
 end
